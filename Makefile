7e3514bc85bce807cda8a2127316e8b07e1cfbac
Suhwan Kim <suhkim@c4r8s2.42seoul.kr>
2023-08-26 22:01:27 +0900

    VM 에서 돌ㄹ아가는 버전

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 all:
-	mkdir ./srcs/db-volume ./srcs/wp-volumes
-	docker-compose -f ./srcs/docker-compose.yml up -d
+	sudo mkdir -p /home/suhkim/data/wordpress
+	sudo mkdir -p /home/suhkim/data/mariadb
+	sudo docker-compose -f ./srcs/docker-compose.yml up --build -d
 
 fclean:
-	docker-compose -f ./srcs/docker-compose.yml down --rmi all --volumes
-	docker system prune -a
-	rm -rf ./srcs/db-volume ./srcs/wp-volume
+	sudo docker-compose -f ./srcs/docker-compose.yml down --rmi all --volumes
+	sudo docker system prune -a
+	sudo rm -rf ./srcs/db-volume ./srcs/wp-volume
 
 re:
-	make fclean
-	make all
+	sudo make fclean
+	sudo make all
 
 .PHONY: all fclean re
--- srcs/requirements/wordpress/tools/wp-script.sh
+++ srcs/requirements/wordpress/tools/wp-script.sh
@@ -3,23 +3,11 @@
 if [ ! -f "/var/www/html/index.php" ]; then
 	wp core download --locale=ko_KR --allow-root --path=/var/www/html
 
-wp core install \
-   --allow-root \
-   --path=${WORDPRESS_PATH} \
-   --url=${WORDPRESS_URL} \
-   --title=Inception \
-   --admin_user=${WORDPRESS_ADMIN} \
-   --admin_password=${WORDPRESS_ADMIN_PASSWORD} \
-   --admin_email=${WORDPRESS_ADMIN_EMAIL} \
-   --skip-email
+	wp core install --allow-root --path=${WORDPRESS_PATH} --url=${WORDPRESS_URL} --title=Inception --admin_user=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --skip-email
 
+	cd /var/www/html
 
-cd /var/www/html
-
-wp user create ${WORDPRESS_USER} \
-               ${WORDPRESS_USER_EMAIL} \
-               --user_pass=${WORDPRESS_USER_PASSWORD} \
-               --role=author --allow-root
+	wp user create ${WORDPRESS_USER} ${WORDPRESS_USER_EMAIL} --user_pass=${WORDPRESS_USER_PASSWORD} --role=author --allow-root
 
 fi
 
